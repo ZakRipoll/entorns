43903ea5885f83c575c0f3ab51b62fbe37f4f378
Adriana <adriana.barrachina01@estudiant.upf.edu>
2017-02-14 19:43:33 +0100

    Pla posicionat mirar de transformar millor les classes

--- js/scene.js
+++ js/scene.js
@@ -3,11 +3,13 @@
 // standard global variables
 var container, scene, camera, renderer, controls, stats;
 var keyboard = new KeyboardState();
-var clock = new THREE.Clock();
+var deltaTime = new THREE.Clock();
 
 // custom global variables
 var mesh;
-var person;
+
+var dimensio = 10;
+var altitude = 15;
 
 function init()
 {	
@@ -15,17 +17,13 @@ function init()
 	
 	container = document.getElementById( "ThreeJS" );
 
-	var SCREEN_WIDTH = window.innerWidth, SCREEN_HEIGHT = window.innerHeight;
+	var SCREEN_WIDTH = container.clientWidth, SCREEN_HEIGHT = container.clientHeight;
 	
 	camera = createCamera( 45, SCREEN_WIDTH / SCREEN_HEIGHT, .1, 2000);
-	camera.position.set( 0, 1.0, 4.0 );
 
-	person = new THREE.Object3D();
-	person.add(camera);
+	scene.add( camera );
 
 	viewSet( view.top );
-
-	scene.add(person);
 
 	if ( Detector.webgl )
 		renderer = new THREE.WebGLRenderer( {antialias:true} );
@@ -45,19 +43,9 @@ function init()
 
 	scene.add( createAxis() );
 
-	var squareT 	= new THREE.ImageUtils.loadTexture("imatges/square-thick.png");
-	squareT.wrapS 	= squareT.wrapT = THREE.RepeatWrapping;
-	squareT.repeat.set(32,32);
+	scene.add( createPlane() );
 
-	this.planeGeo 	= new THREE.PlaneGeometry(32,32);
-	this.planeMat 	= new THREE.MeshBasicMaterial({map:squareT, color:0xbbbbbb});
-	this.basePlane 	= new THREE.Mesh(planeGeo, planeMat);
-
-	basePlane.rotation.x = -Math.PI / 2;
-	basePlane.position.set(16,0,16);
-	basePlane.base = true;
-
-	scene.add(basePlane);
+	camera.lookAt( scene.getObjectByName( "tauler" ).position );
 };
 
 /* ********************************************************* CAMERA ********************************************************* */
@@ -70,21 +58,13 @@ function viewSet( position )
 {
 	switch( position )
 	{
-		case view.ground: // on ground near origin
-
-		person.position.set(-3, 3, 6);
-		person.rotation.set(0, -Math.PI / 2.0, 0); 
-
-		camera.rotation.set(-Math.PI / 16.0, 0, 0); 
+		case view.ground:
 
 		break; 
 
-		case view.top: // birds-eye view
+		case view.top:
 
-		person.position.set(16, 42, 16);
-		person.rotation.set(0, -Math.PI / 2.0, 0); 
-
-		camera.rotation.set(-1.48, 0, 0); 
+		camera.position.set( 0, 15, 0 );
 
 		break;
 
@@ -103,17 +83,43 @@ function createLight( x, y, z, color )
 /* ********************************************************** AXIS ********************************************************** */
 function createAxis()
 {
-	var axis = new THREE.AxisHelper(33);
+	var axis = new THREE.AxisHelper( dimensio );
 	axis.position.y = 0.01;
 	return axis;
+};
+
+function createPlane()
+{
+	var squareT 	= new THREE.ImageUtils.loadTexture("imatges/square-thick.png");
+	squareT.wrapS 	= squareT.wrapT = THREE.RepeatWrapping;
+	squareT.repeat.set( dimensio, dimensio );
+
+	var planeGeo 	= new THREE.PlaneGeometry( dimensio, dimensio );
+	var planeMat 	= new THREE.MeshBasicMaterial({map:squareT, color:0xbbbbbb});
+	var basePlane 	= new THREE.Mesh(planeGeo, planeMat);
+
+	basePlane.rotation.x = -Math.PI / 2;
+	basePlane.name = "tauler";
+
+	return basePlane;
 };
 
 /* ********************************************************* SKYBOX ********************************************************* */
 function createSkybox()
 {
 	var skyBoxGeometry = new THREE.CubeGeometry( 1000, 1000, 1000 );
-	var skyBoxMaterial = new THREE.MeshBasicMaterial( { color: 0xffffee, side: THREE.BackSide } );
+	var skyBoxMaterial = new THREE.MeshBasicMaterial( { color: 0x4bbcfa, side: THREE.BackSide } );
 	return new THREE.Mesh( skyBoxGeometry, skyBoxMaterial );
+};
+
+/* ********************************************************* WAVES ********************************************************* */
+function wobble()
+{
+	for(var i=0; i < verts.length; i++)
+		verts[i].z = Math.sin( deltaTime );
+
+	geometry.verticesNeedUpdate = true;
+	geometry.colorsNeedUpdate = true;
 };
 
 /* ********************************************************* RENDER ********************************************************* */
