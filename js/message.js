dd2cb19eeb1fc2a7bc85c02ab81080c39ab255d0
Adriana <adriana.barrachina01@estudiant.upf.edu>
2017-03-06 13:48:59 +0100

    Green: shoots

--- js/message.js
+++ js/message.js
@@ -2,9 +2,9 @@ var message =
 {
   shoot: function( x, z )
   {
-    x = ( x.toLowerCase().charCodeAt(0) - 97 ) * 50 - 450;
-    z = --z * 50 - 450;
-    //console.log( x + ", " + z);
+    x = ( x.toLowerCase().charCodeAt(0) - 97 ) * 100 - 450;
+    z = --z * 100 - 450;
+    console.log( x + ", " + z);
     scene.add( debug.createCubeRay( x, 50, z, 1, 4 ) );
   }
 }
